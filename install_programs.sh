17b66f7d593837d18a8d783a1d12bb212919c02f
amsterdam <amsterdam@amsterdam.amsterdam>
2023-05-10 00:00:59 -0500

    agregando lineas para la instalacion de Virtualbox

--- install_programs.sh
+++ install_programs.sh
@@ -22,13 +22,25 @@ sudo snap install remmina;
 sudo systemctl enable snapd.apparmor;
 
 #instalacion ANYDESK
-sudo su;
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -;
 echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list;
 gpg --keyserver keyserver.ubuntu.com --recv 18DF3741CDFFDE29;
 gpg --armor --export 18DF3741CDFFDE29 | sudo apt-key add -;
-apt-get update;
-apt-get install anydesk;
+sudo apt-get update;
+sudo apt-get install anydesk;
+
+#instalacion de virtualbox
+sudo apt update
+sudo apt full-upgrade -y
+curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/oracle_vbox_2016.gpg
+curl -fsSL https://www.virtualbox.org/download/oracle_vbox.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/oracle_vbox.gpg
+echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian bullseye contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+sudo apt update
+sudo apt install -y dkms
+sudo apt install -y virtualbox virtualbox-ext-pack
+
 
 #reinicio
-reboot
+[ -f /var/run/reboot-required ] && sudo reboot -f
+
+
